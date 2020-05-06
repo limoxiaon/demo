80d6f12f5c88b100a900e21d2020c5b3a57dfac3
jun_moxiao <li_moxiao@126.com>
2020-05-06 15:26:52 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld1");
+		System.out.println("HelloWorld1");
+		System.out.println("HelloWorld1");
+		System.out.println("HelloWorld1");
+		System.out.println("HelloWorld1");
+	}
+}
